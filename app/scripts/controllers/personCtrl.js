96b743fef06cf463e2496e4aec3c57c161ec4fda
Pascal Guilcher <guilcher.p@sfeir.com>
2013-06-06 17:07:18 +0200

    ..

--- app/scripts/controllers/personCtrl.js
+++ app/scripts/controllers/personCtrl.js
@@ -2,4 +2,9 @@ function PersonCtrl($scope) {
 
     console.log('ctrl-person ' + $scope.person.name);
 
+    $scope.save = function() {
+        var p = $scope.person;
+
+        window.DB_persons[p.id] = p;
+    }
 }
